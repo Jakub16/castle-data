b15ad5b6a1de60c27ed84e8ea5419b612226a7dc
kubaz <jakub.zawadzki16@gmail.com>
2022-12-22 20:18:54 +0100

    Initial Commit

---
+++ src/main/java/com/castle/data/CastleDataApplication.java
@@ -0,0 +1,13 @@
+package com.castle.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CastleDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/castle/data/CastleDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castle.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastleDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
