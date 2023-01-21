06bf98e0d4f7a00f6a8515a84c15fd6209508764
kubaz <jakub.zawadzki16@gmail.com>
2023-01-21 14:34:18 +0100

    Repeater tool created and tested successfully

--- src/test/java/com/castle/data/CastleDataApplicationTests.java
+++ src/test/java/com/castle/data/CastleDataApplicationTests.java
@@ -1,12 +1,12 @@
 package com.castle.data;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+/*import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;*/
 
-@SpringBootTest
+//@SpringBootTest
 class CastleDataApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
