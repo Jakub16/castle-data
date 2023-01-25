28b278a7aa7f1e4c341d52440409be5cffdf7025
kubaz <jakub.zawadzki16@gmail.com>
2023-01-25 22:14:48 +0100

    Changes in web-api module. Introduced mappers (from entities to dtos), created dto classes

--- src/main/java/com/castle/data/repositories/WeatherRepository.java
+++ src/main/java/com/castle/data/repositories/WeatherRepository.java
@@ -2,6 +2,8 @@ package com.castle.data.repositories;
 
 import com.castle.data.model.Weather;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
+    Weather findFirstByOrderByUnixTimeDesc();
 }
